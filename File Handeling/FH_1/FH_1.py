3cdf365cd9facfcddf1f1b6e3e2f29774a30b36d
Rabinxt <brodebid@gmail.com>
2025-08-05 21:44:56 +0545

    updated project, file handeling

--- File Handeling/FH_1/FH_1.py
+++ File Handeling/FH_1/FH_1.py
@@ -1,3 +1,25 @@
-f = open("Rabin.txt", "x")
-f.write("Rabin Chhatkuli")
-f.close()
+class StudentRecordSystem:
+    def __init__(self , filename ="Student.txt"):
+        self.filename = filename
+
+    def add_student(self , name , marks):
+        try:
+            with open(self.filename , "a") as file:
+                file.write(f"{name},{marks}\n")
+                print(f"Student '{name}' added successfully!")
+        except Exception as e:
+                print(f"❌ Error while adding student: {e}")
+    
+    def view_students(self):
+         try:
+            with open(self.filename , "r") as file:
+                data = file.readlines()
+                if not data:
+                    print("No data Found....")
+                    return
+                print("\n📋 Student Records:")
+                for line in data:
+                    name , marks = line.strip().split(",")
+                    print(f"Name: {name}, Marks: {marks}")
+         except FileNotFoundError as e:
+              print("File not found..")
