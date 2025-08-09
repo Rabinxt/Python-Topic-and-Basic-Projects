2dd0f271ab8af5989c619cb155410f8a2905421a
Rabinxt <brodebid@gmail.com>
2025-08-09 23:31:36 +0545

    pratice for mode and packages

---
+++ Modules and Packages/Math Tools/calculator.py
@@ -0,0 +1,8 @@
+def add_number(a,b):
+    return a+b
+def subtract_number(a,b):
+    return a-b
+def multiply_number(a,b):
+    return a*b
+def divide_number(a,b):
+    return a/b
---
+++ Modules and Packages/Math Tools/geometry.py
@@ -0,0 +1,25 @@
+# Circle
+def area_circle(r):
+    pi = 3.14
+    area = r**2*pi
+    return area
+
+def perimeter_circle(r):
+    pi = 3.14
+    perimeter = 2*r*pi
+    return perimeter
+
+# Square
+
+def area_square(side):
+    return side * side
+def perimeter_square(side):
+    return side * 4
+
+# Rectangle
+
+def area_rectangle(a,b):
+    return a*b
+
+def perimeter_rectangle(a,b):
+    return 2*(a+b)
