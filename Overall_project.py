5345e71e1bda50990b8bcd30b7403417da581b81
Rabinxt <brodebid@gmail.com>
2025-08-10 23:05:16 +0545

    updated project

---
+++ Overall_project.py
@@ -0,0 +1,7 @@
+# 5. Mini Project Using All Above
+# Example: Student Report System
+# Reads data from JSON/CSV
+# Calculates statistics (mean, median, mode)
+# Uses error handling
+# Has multiple modules
+# # Uses file handling for reports
