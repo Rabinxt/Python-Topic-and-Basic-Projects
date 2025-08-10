864fe1bcbbff278c0a4390b8ef70a291c77cbbf5
Rabinxt <brodebid@gmail.com>
2025-08-10 23:01:16 +0545

    updated

--- Modules and Packages/MathTools/__init__.py
+++ Modules and Packages/MathTools/__init__.py
@@ -1,6 +1,6 @@
 from .calculator import add_number, subtract_number, multiply_number, divide_number
 from .geometry import (area_circle, perimeter_circle , area_rectangle, perimeter_rectangle, area_square, perimeter_square)
-from .statistics import mean , median
+from .statistics import mean , median , mode
 
 
 __all__ =  [
@@ -8,5 +8,5 @@ __all__ =  [
     "area_circle", "perimeter_circle",
     "area_square", "perimeter_square",
     "area_rectangle", "perimeter_rectangle",
-    "mean", "median"
+    "mean", "median", "mode"
 ]
---
+++ Modules and Packages/Math_tool_MAP_1.py
@@ -0,0 +1,7 @@
+from MathTools import *
+
+
+data = [1,2,3,4,5,6,7]
+print(median(data))
+print(mode(data))
+print(mean(data))
