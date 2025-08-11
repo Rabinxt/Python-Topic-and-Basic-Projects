d00a1007b3b7dd33d1506739f0a0508dfe3cad02
Rabinxt <brodebid@gmail.com>
2025-08-11 23:14:46 +0545

    simple update

--- JSON, CSV, and External APIs/JSON_EX.py
+++ JSON, CSV, and External APIs/JSON_EX.py
@@ -1,7 +1,13 @@
-{
-    "name":"rabin",
-    "age": 19,
-    "skills": ["Python", "Machine Learning", "Data Analysis"],
-    "is_student":True
+# json import
+import json
 
+# json example
+json_data = {
+    "name": "Rabin",
+    "age": 22,
+    "skills": ["Python", "Machine Learning", "Data Analysis"],
+    "is_student": True
 }
+
+# Convert JSON → Python
+python_data = json.loads(json_data)
