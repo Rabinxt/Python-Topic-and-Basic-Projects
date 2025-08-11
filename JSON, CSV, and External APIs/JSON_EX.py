9ff893757857b1a80bc1d26933bbc9cb27fa658c
Rabinxt <brodebid@gmail.com>
2025-08-11 23:18:25 +0545

    up concept

--- JSON, CSV, and External APIs/JSON_EX.py
+++ JSON, CSV, and External APIs/JSON_EX.py
@@ -8,6 +8,20 @@ json_data = {
     "skills": ["Python", "Machine Learning", "Data Analysis"],
     "is_student": True
 }
+python_obj = {"city": "Kathmandu", "country": "Nepal"}
 
 # Convert JSON → Python
 python_data = json.loads(json_data)
+
+# Convert Python → JSON
+json_str = json.dumps(python_obj, indent=4)
+print(json_str)
+
+# Write to JSON file
+with open("students.json", "w") as file:
+    json.dump(json_data, file, indent=4)
+
+# Read from JSON file
+with open("students.json", "r") as file:
+    loaded_data = json.load(file)
+print(loaded_data)
