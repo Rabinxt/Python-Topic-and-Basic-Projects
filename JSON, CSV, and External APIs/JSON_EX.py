aa3fc7cdbac8c4a43bd30f255f5b81581bbfb31c
Rabinxt <brodebid@gmail.com>
2025-08-12 17:39:02 +0545

    updated JSON_EX.py

--- JSON, CSV, and External APIs/JSON_EX.py
+++ JSON, CSV, and External APIs/JSON_EX.py
@@ -11,9 +11,17 @@ json_data = {
 python_obj = {"city": "Kathmandu", "country": "Nepal"}
 
 # Convert JSON → Python
+# 1. Why convert JSON → Python?
+# JSON is the format used for storing or sending data (files, APIs, etc.).
+# When we read JSON, it’s just a string in Python.
+# To work with it (loop, index, update values), we need to turn it into Python data types like dict or list.
 python_data = json.loads(json_data)
 
 # Convert Python → JSON
+# 2. Why convert Python → JSON?
+# When saving or sending data to another program, database, or API, they don’t understand Python’s objects — they expect a standard format like JSON.
+
+# JSON is language-independent — JavaScript, Python, Java, etc., all understand it.
 json_str = json.dumps(python_obj, indent=4)
 print(json_str)
 
