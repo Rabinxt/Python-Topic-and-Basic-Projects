6b8db8f2644b6909078e4a4e60a505f9a804df28
Rabinxt <brodebid@gmail.com>
2025-08-18 21:05:07 +0545

    updated qrproject

--- QR Code Generator/QR_CODE_1.py
+++ QR Code Generator/QR_CODE_1.py
@@ -1,19 +1,13 @@
 import qrcode
 
-# Data to encode
-data = "https://example.com"
 
-# Create QR Code
-qr = qrcode.QRCode(
-    version=1,  # Controls the size of the QR Code (1 is the smallest, 40 is the largest)
-    error_correction=qrcode.constants.ERROR_CORRECT_L,  # Error correction level
-    box_size=10,  # Size of each box in the QR code grid
-    border=4,  # Thickness of the border (minimum is 4)
-)
+Qr = qrcode.QRCode(version=1,
+                   error_correction = qrcode.constants.ERROR_CORRECT_H,
+                   box_size=10,
+                   border=2,
+                   )
 
-qr.add_data(data)
-qr.make(fit=True)
-
-# Create and save the image
-img = qr.make_image(fill_color="black", back_color="white")
-img.save("qrcode.png")
+Qr.add_data("rabinchhatkuli.com.np")
+Qr.make(fit=True)
+img = Qr.make_image(fill_color = "white", back_color = "blue")
+img.save(stream="./QR Code Generator/img.png",)
