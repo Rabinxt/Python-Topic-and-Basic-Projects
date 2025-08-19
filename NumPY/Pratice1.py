13c00e0eee98419d148c7495bd807fa6049eb950
Rabinxt <brodebid@gmail.com>
2025-08-19 23:51:14 +0545

    first numpy project

--- NumPY/Pratice1.py
+++ NumPY/Pratice1.py
@@ -1,2 +1,11 @@
 import numpy as np
 
+arr1 = np.array([1,2,3,4])
+print(arr1)
+
+arr2 = np.array([[1,2,3,4],[5,6,7,8]])
+print(arr2)
+
+arr3 = np.array([[[1,2,3],[4,5,6]],[[7,8,9],[1,2,3]]])
+print(arr3)
+print(arr3.ndim)
