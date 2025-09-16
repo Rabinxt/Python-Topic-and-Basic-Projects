a056ddbba63d822bfbe84d2bfdff06b3422b2025
Rabinxt <brodebid@gmail.com>
2025-09-16 22:36:18 +0545

    acessing own name

--- NumPY/day1_basics.py
+++ NumPY/day1_basics.py
@@ -1,7 +1,14 @@
 import numpy
 
-array = numpy.array([[[1,2,3],[1,2,3]],
-[[2,3,4],[5,6,7]],
+array = numpy.array(
+    [
+        [['A','B','C'],['D','E','F'],['G','H','I']],
+        [['J','K','L'],['M','N','O'],['P','Q','R']],
+        [['S','T','U'],['V','W','X'],['Y','Z','']],
+    ]
+)
 
-])
 print(array.ndim)
+print(array.shape)
+word = array[1,2,2] + array[0,0,0] + array[0,0,1] + array[0,2,2] + array[1,1,1]
+print(word)
