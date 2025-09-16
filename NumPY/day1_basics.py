04a78d547ce683cc67eca4643c35417a83fee599
Rabinxt <brodebid@gmail.com>
2025-09-16 22:43:06 +0545

    updated

--- NumPY/day1_basics.py
+++ NumPY/day1_basics.py
@@ -1,14 +1,20 @@
 import numpy
 
-array = numpy.array(
-    [
-        [['A','B','C'],['D','E','F'],['G','H','I']],
-        [['J','K','L'],['M','N','O'],['P','Q','R']],
-        [['S','T','U'],['V','W','X'],['Y','Z','']],
-    ]
-)
+# array1 = numpy.array(
+#     [
+#         [['A','B','C'],['D','E','F'],['G','H','I']],
+#         [['J','K','L'],['M','N','O'],['P','Q','R']],
+#         [['S','T','U'],['V','W','X'],['Y','Z','']],
+#     ]
+# )
 
-print(array.ndim)
-print(array.shape)
-word = array[1,2,2] + array[0,0,0] + array[0,0,1] + array[0,2,2] + array[1,1,1]
-print(word)
+# print(array1.ndim)
+# print(array1.shape)
+# word = array1[1,2,2] + array1[0,0,0] + array1[0,0,1] + array1[0,2,2] + array1[1,1,1]
+# print(word)
+
+# array2 = numpy.zeros((3,3))
+# print(array2)
+
+array3 = numpy.ones((3,3))
+print(array3)
