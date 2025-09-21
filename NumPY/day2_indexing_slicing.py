2994ae7c471c4a9a60d897af899227b282c1cd99
Rabinxt <brodebid@gmail.com>
2025-09-21 22:03:47 +0545

    slicing completed

--- NumPY/day2_indexing_slicing.py
+++ NumPY/day2_indexing_slicing.py
@@ -1,7 +1,13 @@
 import numpy as np
 
-array1 = np.array([[1, 2, 3, 4],[5, 6, 7, 8],[9, 10, 11, 12],[13, 14, 15, 16]])
+array1 = np.array([[1, 2, 3, 4],
+[5, 6, 7, 8],
+[9, 10, 11, 12],
+[13, 14, 15, 16]])
 
 #array[start:end:step]
 #Starts at 0 index , end in 1 index , with 1 step
 print(array1[0:1:1])
+print(array1[2:0:-1])
+
+print(array1[2:,2:])
