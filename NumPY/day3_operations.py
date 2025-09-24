d4f9d77900aa06b44be900f55d78554a9a4c3061
Rabinxt <brodebid@gmail.com>
2025-09-24 22:35:58 +0545

    updated

--- NumPY/day3_operations.py
+++ NumPY/day3_operations.py
@@ -1,16 +1,22 @@
 import numpy as np
 
-
 array = np.array([1,2,3])
-print(array + 1)
-print(array - 1)
-print(array / 2)
-print(array * 2)
-print(array ** 2)
+print(array + 1) #add
+print(array - 1) #subtraction
+print(array / 2) #Division
+print(array * 2) #Multiplication
+print(array ** 2) #Power
 
+
+#To find square
 print(np.sqrt(array))
 
 array2 = np.array([1.33 , 3.55 , 6.55])
 print(np.round(array2))
 
+#inbuilt pi function
 print(np.pi)
+
+#Finding area of circle Directly
+Radii = np.array([2 , 3, 4])
+print(np.pi * Radii **2 )
