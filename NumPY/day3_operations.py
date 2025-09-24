795e5d164745ed4f0f7fcde8d6344d6e348f79ce
Rabinxt <brodebid@gmail.com>
2025-09-24 22:42:15 +0545

    finish basic arthemetics

--- NumPY/day3_operations.py
+++ NumPY/day3_operations.py
@@ -20,3 +20,13 @@ print(np.pi)
 #Finding area of circle Directly
 Radii = np.array([2 , 3, 4])
 print(np.pi * Radii **2 )
+
+#Element-Wise Arithmetics
+
+array11 = np.array([1, 2, 3])
+array12 = np.array([4, 5, 6])
+print(array11 + array12)
+
+
+scores = np.array([ 13, 1000, 200 , 300, 432 , 56])
+print(scores == 1000)
