ceb7da4810346a140ba2e3f65f8bc98ca4936c24
Rabinxt <brodebid@gmail.com>
2025-09-26 10:07:51 +0545

    multiplication table

--- NumPY/day4_broadcasting.py
+++ NumPY/day4_broadcasting.py
@@ -1,9 +1,17 @@
 import numpy as np
 
-array1 = np.array([[1,2,3,4]])
+array1 = np.array([[1,2,3,4],
+                   [5,6,7,8],
+                   [9,1,2,3],
+                   [4,5,6,7]])
 array2 = np.array([[1],[2],[3],[4]])
 
 print(array1.shape)
 print(array2.shape)
 
 print(array1 * array2)
+
+#creating multiplacation table from 1 to 10
+numbers1 = np.array([[1],[2],[3],[4],[5],[6],[7],[8],[9],[10]])
+numbers2 = np.array([[1,2,3,4,5,6,7,8,9,10]])
+print(numbers2 * numbers1)
