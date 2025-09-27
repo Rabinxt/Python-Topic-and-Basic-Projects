964a9392022b99e675bfc6b00e725a5abc8697c2
Rabinxt <brodebid@gmail.com>
2025-09-27 21:45:11 +0545

    filtering

--- NumPY/day5_array_manipulation.py
+++ NumPY/day5_array_manipulation.py
@@ -12,3 +12,16 @@ twod_array = np.array([[3,4],
 print("Sorted 2D array in column is: ", np.sort(twod_array, axis=0))
 #for sorting in rows
 print("Sorted 2D array in rows: ", np.sort(twod_array, axis=1))
+
+
+#Filtering
+
+numbers = np.array([1,2,3,4,5,6,7,8,9,0])
+even_numbers = numbers[numbers%2 == 0]
+print(even_numbers)
+odd_numbers = numbers[numbers % 2 != 0]
+print(odd_numbers)
+
+#Filtering with mask
+mask = numbers > 5
+print(numbers[mask])
