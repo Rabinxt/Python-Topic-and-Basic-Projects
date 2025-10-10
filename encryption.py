da49a74e0660460ffcae83caaa894b9836b9ec3c
Rabinxt <brodebid@gmail.com>
2025-10-10 21:07:26 +0545

    encryption

--- encryption.py
+++ encryption.py
@@ -1,6 +1,17 @@
 import string
 import random
 
-chars =" "+ string.whitespace +string.punctuation + string.ascii_letters + string.digits
+chars =" " +string.punctuation + string.ascii_letters + string.digits
 chars = list(chars)
-print(chars)
+key = chars.copy()
+random.shuffle(key)
+
+#ENCRYPT
+
+plain_text = input("Enter the message : ")
+cipher_text = ''
+
+for letter in plain_text:
+    index = chars.index(letter)
+    cipher_text += key[index]
+print(f"Cipher Text : {cipher_text}")
