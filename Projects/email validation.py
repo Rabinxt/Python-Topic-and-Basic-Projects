e85c996c5c44f2e02780a0deb284a88e6980c1df
Rabinxt <brodebid@gmail.com>
2025-10-18 20:46:04 +0545

    email validation

--- Projects/email validation.py
+++ Projects/email validation.py
@@ -1,6 +1,12 @@
 email = input("Please input Email to Check: ")
 
 if len(email)>6:
-    pass
+    if email[0].isalpha :
+        if ("@" in email) and (email.count("@") == 1):
+            pass
+        else:
+            print("Wrong Format")
+    else:
+        print("first should be capital")
 else:
     print("Length should be greater than 6 digits")
