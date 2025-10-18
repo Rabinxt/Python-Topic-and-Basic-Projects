d6796b899c0a1d4d0156ea5838622b1ffd8d5115
Rabinxt <brodebid@gmail.com>
2025-10-18 20:30:13 +0545

    api finished

--- Projects/APIPokemon.py
+++ Projects/APIPokemon.py
@@ -6,9 +6,17 @@ def get_pokemon_info(name):
     url = f'{base_url}/pokemon/{name}'
     response = requests.get(url)
     if response.status_code == 200:
-        print("Data retrieved")
+        pokemon_data = response.json()
+        return pokemon_data
+
     else:
         print(f"Data not found error:{response.status_code}")
     
 pokemon_name = 'pikachu'
-get_pokemon_info(pokemon_name)
+pokemon_info = get_pokemon_info(pokemon_name)
+
+if pokemon_info:
+    print(f"Name: {pokemon_info['name']}")
+    print(f"Id : {pokemon_info['id']}")
+    print(f"Height: {pokemon_info['height']}")
+    print(f"abilities: {pokemon_info['abilities']}")
