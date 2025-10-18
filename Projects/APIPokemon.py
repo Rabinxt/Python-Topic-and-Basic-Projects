ce1947b46af1db3bd30092866c72703b8a9f4ece
Rabinxt <brodebid@gmail.com>
2025-10-18 20:18:28 +0545

    api

--- Projects/APIPokemon.py
+++ Projects/APIPokemon.py
@@ -5,7 +5,10 @@ base_url = "https://pokeapi.co/api/v2/"
 def get_pokemon_info(name):
     url = f'{base_url}/pokemon/{name}'
     response = requests.get(url)
-    print(response) 
+    if response.status_code == 200:
+        print("Data retrieved")
+    else:
+        print(f"Data not found error:{response.status_code}")
     
 pokemon_name = 'pikachu'
 get_pokemon_info(pokemon_name)
