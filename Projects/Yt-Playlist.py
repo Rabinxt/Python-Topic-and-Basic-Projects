39f85bb72297128b48598c97165427e8603f4a52
Rabinxt <brodebid@gmail.com>
2025-10-25 10:19:33 +0545

    Yt-Playlist

--- Projects/Yt-Playlist.py
+++ Projects/Yt-Playlist.py
@@ -4,3 +4,9 @@ p = Playlist("your link")
 
 for video in p.videos:
     video.streams.first().download()
+    
+    
+#Or use command prompt 
+#   pip install yt-dlp
+
+# yt-dlp -x --audio-format mp3 <playlist_URL>
